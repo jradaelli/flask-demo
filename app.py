47d7495f0f8dfd67cd7754b36e9e12f7a2781245
jradaelli <j.radaelli@reply.it>
2016-07-27 19:10:14 -0400

    add question 3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from bokeh.embed import components
 
 from plots import *
@@ -53,6 +53,39 @@ def render_q12(df=df):
                            counter=number, surveydt=surveydt,
                            title='Integrating Live Data')
     return encode_utf8(html)
+
+
+@app.route('/question3.csv')
+def data_csv():
+    df = nyc_current()
+    print 'test'
+    return df.iloc[:, -4:-2].to_csv(index=False)
+
+
+@app.route('/question3')
+def show_q3():
+    html = render_template('graph3.html')
+    return encode_utf8(html)
+
+
+@app.route('/positions_at_time.csv')
+def positions_at_time(archive=df):
+    hours = int(request.args.get('hours', 12))
+    minutes = int(request.args.get('minutes', 30))
+
+    # could easily make this parameter modifiable
+    date_for_data = dt.date(2015, 9, 17)
+
+    time_for_data = dt.time(hours, minutes)
+
+    datetime_for_data = dt.datetime.combine(date_for_data, time_for_data)
+
+    # 2 minute window
+    lower_bound = datetime_for_data - dt.timedelta(seconds=60)
+    upper_bound = datetime_for_data + dt.timedelta(seconds=60)
+
+    # JSON would work too, but CSV is actually better for D3
+    return archive[lower_bound:upper_bound].to_csv()
 
 
 if __name__ == '__main__':
