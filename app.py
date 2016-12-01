01e321de697cdca16a911c0abc7e40b2dacddbb6
jradaelli <j.radaelli@reply.it>
2016-12-01 14:26:04 +0100

    'graph'

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 from flask import Flask, render_template, request, redirect
+from utils import *
+from bokeh.embed import components
+from bokeh.util.string import encode_utf8
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def main():
-  return redirect('/index')
+    return redirect('/dashboard')
 
-@app.route('/index')
+
+@app.route('/dashboard')
 def index():
-  return render_template('index.html')
+    df = get_data()
+    logger.info("Data are loaded")
+    q1a = df.vehicle_id.groupby(df.index).count()
+    plot = build_chart(q1a)
+    script1, div1 = components(plot)
+    html = render_template('pages/index.html', script1=script1, div1=div1)
+    return encode_utf8(html)
 
 if __name__ == '__main__':
-  app.run(port=33507)
+    app.run(port=33507)
---
+++ utils.py
@@ -0,0 +1,31 @@
+from bokeh.plotting import *
+import pandas as pd
+import os
+
+
+def date_parser(u):
+    date_format_str = "%Y-%m-%d %H:%M:%S"
+    return pd.datetime.strptime(u, date_format_str)
+
+
+def get_data(date_parser=date_parser):
+    root = os.getcwd()
+    pathdata = os.path.join(root, "static", "data", "manhattan.csv")
+
+    df = pd.read_csv(pathdata,
+                     parse_dates=True,
+                     date_parser=date_parser,
+                     index_col=1,
+                     nrows=100)
+    return df
+
+
+def build_chart(df):
+    x_data = df.index
+    y_data = df
+    plot = figure(x_axis_type="datetime", width=600, height=300,
+                  tools="pan,xwheel_zoom,box_zoom,reset,resize",
+                  title="Vehicle Trend")
+    plot.grid.grid_line_alpha = 0.3
+    plot.line(x_data, y_data)
+    return plot
